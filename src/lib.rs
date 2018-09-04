23484b3f2b2518cc83488c57e8dbc51d5241f2f6
Jessica Stokes <hello@jessicastokes.net>
2018-09-03 17:19:10 -0700

    Refactor to make this testable as a library

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,18 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
-#![feature(custom_derive)]
-
 #[macro_use]
 extern crate html5ever;
 extern crate kuchiki;
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
 extern crate rayon;
 extern crate reqwest;
-extern crate rocket;
-extern crate rocket_contrib;
 extern crate rss;
 
 use kuchiki::Selectors;
 use rayon::prelude::*;
-use rocket::http::RawStr;
-use rocket::request::FromFormValue;
-use rocket::response::content::Xml;
-use rocket::response::Failure;
-use rocket::{Request, State};
-use rocket_contrib::Template;
 use rss::Channel;
 use std::io::BufReader;
-
-const NAME: &'static str = env!("CARGO_PKG_NAME");
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const SOURCE_VERSION: &'static str = env!("SOURCE_VERSION");
-
-#[get("/")]
-fn index() -> Template {
-  let mut map = std::collections::HashMap::new();
-  map.insert("path", "/");
-  Template::render("index", &map)
-}
-
-struct HTTPClient {
-  client: reqwest::Client,
-}
+use std::result::Result;
 
 fn create_br_element() -> kuchiki::NodeRef {
   kuchiki::NodeRef::new_element(
@@ -44,49 +21,24 @@ fn create_br_element() -> kuchiki::NodeRef {
   )
 }
 
-struct CSSSelector(Selectors);
-
-impl<'v> FromFormValue<'v> for CSSSelector {
-  type Error = &'v RawStr;
-
-  fn from_form_value(form_value: &'v RawStr) -> Result<CSSSelector, &'v RawStr> {
-    form_value
-      .url_decode()
-      .or_else(|_| Err(form_value))
-      .and_then(|decoded| {
-        Selectors::compile(&decoded)
-          .or_else(|_| Err(form_value))
-          .and_then(|selector| Ok(CSSSelector(selector)))
-      })
-  }
-}
-
-#[derive(FromForm)]
-struct FeedConfiguration {
-  feed: String,
-  description_selector: CSSSelector,
-}
-
-#[get("/refurb?<configuration>")]
-fn refurb(
-  configuration: FeedConfiguration,
-  http_client: State<HTTPClient>,
-) -> Result<Xml<String>, Failure> {
+pub fn refurb(
+  feed_url: String,
+  description_selector: Selectors,
+  http_client: &reqwest::Client,
+) -> Result<Channel, String> {
   let mut feed = {
-    let response_buffer = match http_client.client.get(configuration.feed.as_str()).send() {
-      Err(_error) => {
-        // TODO: Handle specific errors
-        return Err(Failure(rocket::http::Status::NotAcceptable));
+    let response_buffer = match http_client.get(feed_url.as_str()).send() {
+      Err(error) => {
+        return Err(error.to_string());
       }
       Ok(response) => BufReader::new(response),
     };
 
-    println!("Fetched {}", configuration.feed);
+    println!("Fetched {}", feed_url);
 
     match Channel::read_from(response_buffer) {
-      Err(_error) => {
-        // TODO: Handle specific errors
-        return Err(Failure(rocket::http::Status::NotAcceptable));
+      Err(error) => {
+        return Err(error.to_string());
       }
       Ok(parsed) => parsed,
     }
@@ -109,7 +61,7 @@ fn refurb(
 
       let source_document = {
         let text = {
-          let mut response = match http_client.client.get(url.as_str()).send() {
+          let mut response = match http_client.get(url.as_str()).send() {
             Err(_error) => return,
             Ok(response) => response,
           };
@@ -145,9 +97,7 @@ fn refurb(
 
         let target_document = kuchiki::NodeRef::new_document();
 
-        let selected = configuration
-          .description_selector
-          .0
+        let selected = description_selector
           .filter(source_document.descendants().elements())
           .collect::<Vec<_>>();
 
@@ -182,35 +132,110 @@ fn refurb(
 
   println!("Processed entire feed!");
 
-  Ok(Xml(feed.to_string()))
+  Ok(feed)
 }
 
-#[catch(404)]
-fn not_found(req: &Request) -> Template {
-  let mut map = std::collections::HashMap::new();
-  map.insert("path", req.uri().as_str());
-  Template::render("error/404", &map)
-}
+#[cfg(test)]
+mod test {
+  extern crate mockito;
+  extern crate reqwest;
+  extern crate rss;
 
-fn shared_http_client() -> HTTPClient {
-  use reqwest::header;
-  use reqwest::Client;
+  use kuchiki::Selectors;
 
-  let mut headers = header::Headers::new();
-  headers.set(header::UserAgent::new(format!(
-    "{}/{} ({})",
-    NAME, VERSION, SOURCE_VERSION
-  )));
+  use super::refurb;
 
-  HTTPClient {
-    client: Client::builder().default_headers(headers).build().unwrap(),
+  fn mock_get_request(path: &str, content_type: &str, body: &str) -> mockito::Mock {
+    mockito::mock("GET", path)
+      .with_header("content-type", content_type)
+      .with_body(body)
+      .create()
   }
-}
 
-pub fn application() -> rocket::Rocket {
-  rocket::ignite()
-    .attach(Template::fairing())
-    .manage(shared_http_client())
-    .mount("/", routes![index, refurb])
-    .catch(catchers![not_found])
+  #[test]
+  fn refurb_returns_valid_feed() {
+    let server_host = format!("http://{}", mockito::SERVER_ADDRESS);
+    let feed_path = "/feed.rss";
+    let article_path = "/articles/latest-cool-article-123";
+
+    let feed_request = mock_get_request(
+      feed_path,
+      "application/xml",
+      &format!(
+        r#"<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+  <channel>
+    <title>My Test Feed</title>
+    <link>{host}</link>
+    <description>Test feed for testing purposes!</description>
+    <language>en-us</language>
+    <item>
+      <title><![CDATA[My Latest Cool Article]]></title>
+      <description><![CDATA[bad and not good article summary, click for more]]></description>
+      <link>{host}{article}</link>
+      <author>webmaster@{hostname}</author>
+      <pubDate>Fri, 31 Aug 2018 03:59:52 +0000</pubDate>
+      <guid>{host}{article}</guid>
+    </item>
+  </channel>
+</rss>
+"#,
+        hostname = mockito::SERVER_ADDRESS,
+        host = server_host,
+        article = article_path,
+      ),
+    );
+
+    let article_request = mock_get_request(article_path, "text/html", &r#"<!DOCTYPE html>
+<head>
+  <title>My Latest Cool Article</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/articles">Articles</a>
+  </nav>
+  <section>
+    <h1>My Latest Cool Article</h1>
+    <img class="main-image" src="/images/latest-cool-article-123-main.jpg" />
+    <article>
+      <p>Here is my latest cool article. It's very good and full of cool and useful information.</p>
+      <p>My RSS feed is truncated, so you'd better click my links! I'd better see you in Google Analytics.</p>
+    </article>
+  </section>
+</body>"#);
+
+    let client = reqwest::Client::new();
+
+    let expected_feed = rss::Channel::read_from(format!(
+      r#"<rss version="2.0"><channel><title>My Test Feed</title><link>{host}</link><description>Test feed for testing purposes!</description><language>en-us</language><item><title>My Latest Cool Article</title><link>{host}{article}</link><description><![CDATA[<img class="main-image" src="/images/latest-cool-article-123-main.jpg"><br><article>
+      <p>Here is my latest cool article. It's very good and full of cool and useful information.</p>
+      <p>My RSS feed is truncated, so you'd better click my links! I'd better see you in Google Analytics.</p>
+    </article>]]></description><author>webmaster@{hostname}</author><guid>{host}{article}</guid><pubDate>Fri, 31 Aug 2018 03:59:52 +0000</pubDate></item></channel></rss>"#,
+      hostname = mockito::SERVER_ADDRESS,
+      host = server_host,
+      article = article_path,
+    ).as_bytes());
+
+    let processed_feed = refurb(
+      format!("{}{}", server_host, feed_path),
+      Selectors::compile(".main-image,article").expect("compiled selectors"),
+      &client,
+    );
+
+    assert!(
+      processed_feed.is_ok(),
+      "should return success processing feed"
+    );
+
+    assert_eq!(
+      processed_feed.expect("processed feed"),
+      expected_feed.expect("expected feed"),
+      "should return the expected feed"
+    );
+
+    feed_request.assert();
+    article_request.assert();
+  }
 }
