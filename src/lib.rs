5f28125731dd74dd04d6ee71206ec4f2df688a1e
Jessica Stokes <hello@jessicastokes.net>
2018-09-03 17:56:00 -0700

    Remove mock_get_request shortcut

--- src/lib.rs
+++ src/lib.rs
@@ -145,35 +145,26 @@ mod test {
 
   use super::refurb;
 
-  fn mock_get_request(path: &str, content_type: &str, body: &str) -> mockito::Mock {
-    mockito::mock("GET", path)
-      .with_header("content-type", content_type)
-      .with_body(body)
-      .create()
-  }
-
   #[test]
   fn refurb_returns_valid_feed() {
     let server_host = format!("http://{}", mockito::SERVER_ADDRESS);
     let feed_path = "/feed.rss";
     let article_path = "/articles/latest-cool-article-123";
 
-    let feed_request = mock_get_request(
-      feed_path,
-      "application/xml",
-      &format!(
+    let feed_request = mockito::mock("GET", feed_path)
+      .with_header("content-type", "application/xml")
+      .with_body(&format!(
         include_str!("../test/fixtures/refurb_returns_valid_feed/source-feed.interpolated.xml"),
         hostname = mockito::SERVER_ADDRESS,
         host = server_host,
         article = article_path,
-      ),
-    );
+      )).create();
 
-    let article_request = mock_get_request(
-      article_path,
-      "text/html",
-      &include_str!("../test/fixtures/refurb_returns_valid_feed/latest-cool-article-123.html"),
-    );
+    let article_request = mockito::mock("GET", article_path)
+      .with_header("content-type", "text/html")
+      .with_body(&include_str!(
+        "../test/fixtures/refurb_returns_valid_feed/latest-cool-article-123.html"
+      )).create();
 
     let client = reqwest::Client::new();
 
