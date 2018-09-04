46fe5f224c06c6429ad19e1ac2439e4fab17b49a
Jessica Stokes <hello@jessicastokes.net>
2018-09-03 17:32:10 -0700

    Move fixtures out of source files for maintainability

--- src/lib.rs
+++ src/lib.rs
@@ -162,61 +162,29 @@ mod test {
       feed_path,
       "application/xml",
       &format!(
-        r#"<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-  <channel>
-    <title>My Test Feed</title>
-    <link>{host}</link>
-    <description>Test feed for testing purposes!</description>
-    <language>en-us</language>
-    <item>
-      <title><![CDATA[My Latest Cool Article]]></title>
-      <description><![CDATA[bad and not good article summary, click for more]]></description>
-      <link>{host}{article}</link>
-      <author>webmaster@{hostname}</author>
-      <pubDate>Fri, 31 Aug 2018 03:59:52 +0000</pubDate>
-      <guid>{host}{article}</guid>
-    </item>
-  </channel>
-</rss>
-"#,
+        include_str!("../test/fixtures/refurb_returns_valid_feed/source-feed.interpolated.xml"),
         hostname = mockito::SERVER_ADDRESS,
         host = server_host,
         article = article_path,
       ),
     );
 
-    let article_request = mock_get_request(article_path, "text/html", &r#"<!DOCTYPE html>
-<head>
-  <title>My Latest Cool Article</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-  <nav>
-    <a href="/">Home</a>
-    <a href="/articles">Articles</a>
-  </nav>
-  <section>
-    <h1>My Latest Cool Article</h1>
-    <img class="main-image" src="/images/latest-cool-article-123-main.jpg" />
-    <article>
-      <p>Here is my latest cool article. It's very good and full of cool and useful information.</p>
-      <p>My RSS feed is truncated, so you'd better click my links! I'd better see you in Google Analytics.</p>
-    </article>
-  </section>
-</body>"#);
+    let article_request = mock_get_request(
+      article_path,
+      "text/html",
+      &include_str!("../test/fixtures/refurb_returns_valid_feed/latest-cool-article-123.html"),
+    );
 
     let client = reqwest::Client::new();
 
-    let expected_feed = rss::Channel::read_from(format!(
-      r#"<rss version="2.0"><channel><title>My Test Feed</title><link>{host}</link><description>Test feed for testing purposes!</description><language>en-us</language><item><title>My Latest Cool Article</title><link>{host}{article}</link><description><![CDATA[<img class="main-image" src="/images/latest-cool-article-123-main.jpg"><br><article>
-      <p>Here is my latest cool article. It's very good and full of cool and useful information.</p>
-      <p>My RSS feed is truncated, so you'd better click my links! I'd better see you in Google Analytics.</p>
-    </article>]]></description><author>webmaster@{hostname}</author><guid>{host}{article}</guid><pubDate>Fri, 31 Aug 2018 03:59:52 +0000</pubDate></item></channel></rss>"#,
-      hostname = mockito::SERVER_ADDRESS,
-      host = server_host,
-      article = article_path,
-    ).as_bytes());
+    let expected_feed = rss::Channel::read_from(
+      format!(
+        include_str!("../test/fixtures/refurb_returns_valid_feed/expected-feed.interpolated.xml"),
+        hostname = mockito::SERVER_ADDRESS,
+        host = server_host,
+        article = article_path,
+      ).as_bytes(),
+    );
 
     let processed_feed = refurb(
       format!("{}{}", server_host, feed_path),
